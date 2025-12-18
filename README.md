00d8effa0348808ec9276e58e37d954b0b9c82a7
pepijn-lens <pepijn.lens@icloud.com>
2025-12-18 14:17:13 +0100

    Add readme and data loading

---
+++ README.md
@@ -0,0 +1,5 @@
+## Setup 
+Make sure to create a virtual environment and install the requirements. 
+
+## Download dataset
+To download the dataset run "python download_data.py" from the project root. 
---
+++ download_data.py
@@ -0,0 +1,12 @@
+import kagglehub
+import os
+import shutil
+
+os.makedirs("data", exist_ok=True)
+
+# Download latest version
+path = kagglehub.dataset_download("sgpjesus/bank-account-fraud-dataset-neurips-2022")
+
+shutil.move(path, "data/bank_account_fraud_dataset.zip")
+
+print("Dataset downloaded and moved to the data folder")   
