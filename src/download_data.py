9d1995766135950d914e94017b870501e6c4b0a8
rbmr <robertmertens10@gmail.com>
2026-01-22 16:57:44 +0100

    update download_data for reusability

--- src/download_data.py
+++ src/download_data.py
@@ -1,13 +1,16 @@
+from pathlib import Path
+
 import kagglehub
 import shutil
 
 from constants import DATA_DIR
 
-DATA_DIR.mkdir(parents=True, exist_ok=True)
+def download_data(output_dir: Path):
+    # Download latest version
+    output_dir.mkdir(parents=True, exist_ok=True)
+    path = kagglehub.dataset_download("sgpjesus/bank-account-fraud-dataset-neurips-2022")
+    shutil.move(path, output_dir)
+    print(f"Dataset downloaded and moved to {output_dir}")
 
-# Download latest version
-path = kagglehub.dataset_download("sgpjesus/bank-account-fraud-dataset-neurips-2022")
-
-shutil.move(path, DATA_DIR / "bank_account_fraud_dataset")
-
-print("Dataset downloaded and moved to the data folder")
+if __name__ == "__main__":
+    download_data(DATA_DIR / "bank_account_fraud_dataset")
